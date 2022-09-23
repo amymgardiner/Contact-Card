e57d3e6d1cd83d44a623d1e059d114f3d9073982
Amy Gardiner <amymgardiner@gmail.com>
2022-09-22 20:04:43 -0500

    created contacts table

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,18 +1,23 @@
 // Import modules
+import { initdb } from './database';
 import './form';
 import './submit';
-
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Import CSS files
 import '../css/index.css';
 
+// Import Bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// On load functionality
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
