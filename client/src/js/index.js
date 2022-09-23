208880d344c9291d80f0836ed68c34d73c40293f
Amy Gardiner <amymgardiner@gmail.com>
2022-09-22 20:58:06 -0500

    second boilerplate code in index.js this time

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 // Import modules
 import { initdb, getDb, postDb } from './database';
-import './form';
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
 import './submit';
 
 // Import CSS files
@@ -18,8 +19,44 @@ import Dog from '../images/dog.png';
 // On load functionality
 window.addEventListener('load', function () {
   initdb();
+  fetchCards();
 
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
+});
+
+// Form functionality
+const form = document.getElementById('formToggle');
+const newContactButton = document.getElementById('new-contact');
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', (event) => {
+  toggleForm();
+});
+
+form.addEventListener('submit', (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById('name').value;
+  let phone = document.getElementById('phone').value;
+  let email = document.getElementById('email').value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
